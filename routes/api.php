b3e5c54dcc8cf47b3cf99ce48143f2d2a5259062
selogerkkk <ruanhborges@gmail.com>
2023-11-21 15:04:21 -0300

    feat(routes): adição de rotas de login e de usuário

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +16,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::post('auth/login', [AuthController::class, 'login']);
+
+Route::group(['middleware' => ['apiJwt']], function () {
+    Route::get('users', [UserController::class, 'index']);
 });
