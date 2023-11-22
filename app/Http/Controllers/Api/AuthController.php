facd561715a2e727187a497db9ca78e6ab751d9c
selogerkkk <ruanhborges@gmail.com>
2023-11-21 23:07:48 -0300

    style(controller): tradução do retorno json

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -17,7 +17,7 @@ class AuthController extends Controller
         $credentials = $request->only(['email', 'password']);
 
         if (!$token = auth('api')->attempt($credentials)) {
-            return response()->json(['error' => 'Unauthorized'], 401);
+            return response()->json(['error' => 'Não autorizado'], 401);
         }
 
         return $this->respondWithToken($token);
