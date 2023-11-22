77683a6c521889201db276b3bd8c4510fb440af6
selogerkkk <ruanhborges@gmail.com>
2023-11-21 23:09:14 -0300

    feat(model): adiciona proteçao na tabela e colunas da tabela

---
+++ app/Models/Tool.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Tool extends Model
+{
+    use HasFactory;
+
+    protected $table = 'Tools';
+    protected $fillable = [
+        'title',
+        'link',
+        'description',
+        'tags'
+    ];
+}
