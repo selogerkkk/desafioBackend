d64684904e0c54b2d7f628c1b79eb8f2060cb118
selogerkkk <ruanhborges@gmail.com>
2023-11-22 21:59:05 -0300

    docs(auth): adiciona documentação Swagger para o endpoint de autenticação

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -5,6 +5,47 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+/**
+ * @OA\Post(
+ *     path="/api/auth/login",
+ *     summary="Autenticação de usuário",
+ *     tags={"Autenticação"},
+ *     security={{"bearer_token": {}}},
+ *     @OA\Parameter(
+ *         name="email",
+ *         in="query",
+ *         description="Endereço de e-mail do usuário",
+ *         required=true,
+ *         @OA\Schema(type="string")
+ *     ),
+ *     @OA\Parameter(
+ *         name="password",
+ *         in="query",
+ *         description="Senha do usuário",
+ *         required=true,
+ *         @OA\Schema(type="string")
+ *     ),
+ *    
+ * @OA\Response(
+ *     response="200",
+ *     description="Token de autenticação",
+ *     @OA\JsonContent(
+ *         type="object",
+ *         @OA\Property(property="access_token", type="string", example="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9..."),
+ *         @OA\Property(property="token_type", type="string", example="bearer"),
+ *         @OA\Property(property="expires_in", type="integer", example=3600),
+ *     )
+ * ),
+ *     @OA\Response(
+ *         response="401",
+ *         description="Não autorizado",
+ *         @OA\JsonContent(
+ *             type="object",
+ *             @OA\Property(property="message", type="string", example="Não autorizado.")
+ *         )
+ *     ),
+ * )
+ **/
 class AuthController extends Controller
 {
     /**
